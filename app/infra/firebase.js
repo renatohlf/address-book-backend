fbb27c6fbc4cde0ff2f2045b43f2ae70c7a972d2
Renato Farias <rhlf@cesar.org.br>
2018-07-21 01:31:52 -0300

    Firebase config

--- app/infra/firebase.js
+++ app/infra/firebase.js
@@ -1,6 +1,14 @@
-var firebase = require('firebase').initializeApp({
-    serviceAccount: "./addressbook-03d0ab92095f.json",
-    databaseURL: "https://addressbook-3a4e5.firebaseio.com/"
-});
+var firebase = require('firebase');
+
+var config = {
+    apiKey: "AIzaSyDsikDb6zhMUAP5eC6UYYRKkFvLL-BjRiI",
+    authDomain: "addressbook-3a4e5.firebaseapp.com",
+    databaseURL: "https://addressbook-3a4e5.firebaseio.com",
+    projectId: "addressbook-3a4e5",
+    storageBucket: "addressbook-3a4e5.appspot.com",
+    messagingSenderId: "120328092777"
+};
+
+firebase.initializeApp(config);
 
 module.exports = firebase;
