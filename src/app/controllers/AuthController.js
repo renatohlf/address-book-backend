e5dabaa811f66f562a7e3328aa2e0d38f66a0b20
Renato Farias <renatohlf@gmail.com>
2020-06-13 00:18:20 +0200

    Adjusting login controller

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -6,26 +6,48 @@ const { PrismaClient } = prismaClient;
 const prisma = new PrismaClient();
 
 async function login(req, res) {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    await prisma.user.findOne({
-        where: { email: username }
-    }).then(async (user) => {
-        if (!await bcrypt.compare(password, user.password)) {
-            return res.status(400).json({ error: "Invalid password" });
-        }
-
-        const token = generateToken({ userId: user.id, email: user.email, name: user.name });
-        user.password = undefined;
-
-        return res.status(200).send({ user, token });
-
-    }).catch( err => {
-    
-        if (err.code === 'P2016') return res.status(404).send("User not found");
-
-        return res.status(400).send({err});
+  try {
+    const user = await prisma.user.findOne({
+      where: { email: username },
     });
+
+    if(!user) return res.status(404).send("User not found");
+
+    if (!await bcrypt.compare(password, user.password)) {
+        return res.status(400).json({ error: "Invalid password" });
+    }
+
+    return res.status(200).send({ user });
+
+  } catch (error) {
+    return res.status(400).send({ error });
+  }
+
+//   await prisma.user
+//     .findOne({
+//       where: { email: username },
+//     })
+//     .then(async (user) => {
+//       if (!await bcrypt.compare(password, user.password)) {
+//         return res.status(400).json({ error: "Invalid password" });
+//       }
+
+//       const token = generateToken({
+//         userId: user.id,
+//         email: user.email,
+//         name: user.name,
+//       });
+//       user.password = undefined;
+
+//       return res.status(200).send({ user, token });
+//     })
+//     .catch((err) => {
+//       if (err.code === "P2016") return res.status(404).send("User not found");
+
+//       return res.status(400).send({ err });
+//     });
 }
 
 export default { login };
