0d8d7869fb8372799f0611087679ddf7d13f2292
Renato Farias <renatohlf@gmail.com>
2020-06-13 00:47:18 +0200

    Adjusting login controller

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -8,53 +8,29 @@ const prisma = new PrismaClient();
 async function login(req, res) {
   const { username, password } = req.body;
 
-  try {
-    const user = await prisma.user.findOne({
+  await prisma.user
+    .findOne({
       where: { email: username },
-    });
-
-    if(!user) return res.status(404).send("User not found");
-
-    if (!await bcrypt.compare(password, user.password)) {
+    })
+    .then(async (user) => {
+      if (!await bcrypt.compare(password, user.password)) {
         return res.status(400).json({ error: "Invalid password" });
-    }
+      }
 
-    const token = generateToken({
+      const token = generateToken({
         userId: user.id,
         email: user.email,
         name: user.name,
+      });
+      user.password = undefined;
+
+      return res.status(200).send({ user, token });
+    })
+    .catch((err) => {
+      if (err.code === "P2016") return res.status(404).send("User not found");
+
+      return res.status(400).send({ err });
     });
-    user.password = undefined;
-
-    return res.status(200).send({ user, token });
-
-  } catch (error) {
-    return res.status(400).send({ error });
-  }
-
-//   await prisma.user
-//     .findOne({
-//       where: { email: username },
-//     })
-//     .then(async (user) => {
-//       if (!await bcrypt.compare(password, user.password)) {
-//         return res.status(400).json({ error: "Invalid password" });
-//       }
-
-//       const token = generateToken({
-//         userId: user.id,
-//         email: user.email,
-//         name: user.name,
-//       });
-//       user.password = undefined;
-
-//       return res.status(200).send({ user, token });
-//     })
-//     .catch((err) => {
-//       if (err.code === "P2016") return res.status(404).send("User not found");
-
-//       return res.status(400).send({ err });
-//     });
 }
 
 export default { login };
